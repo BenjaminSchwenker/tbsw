8f6797056dcc9f5968ae70a17a1bd2477c2f3eaf
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2016-09-19 18:04:44 +0200

    fixup

--- source/TBReco/source/src/StripHitMaker.cc
+++ source/TBReco/source/src/StripHitMaker.cc
@@ -196,7 +196,7 @@ void StripHitMaker::processEvent(LCEvent * evt)
       if (hitU.GetDAQID() == hitV.GetDAQID() ) {
         
         // Make LCIO TrackerHit 
-        TBHit hit(hitU.GetDAQID(), hitU.GetCoord[0][0], hitV.GetCoord[1][0], hitU.GetCov[0][0], hitV.GetCov[1][1], 0);
+        TBHit hit(hitU.GetDAQID(), hitU.GetCoord()[0][0], hitV.GetCoord()[1][0], hitU.GetCov()[0][0], hitV.GetCov()[1][1], 0);
         TrackerHitImpl * trackerhit = hit.MakeLCIOHit();  
             
         // Add link to full cluster data 
