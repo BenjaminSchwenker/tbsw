980c38f61484f0d5ee5b51906532ab2007bfdb64
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2017-03-30 16:21:37 +0200

    update ClusterDBViewer

--- workspace/root-scripts/residuals/ClusterDBViewer.C
+++ workspace/root-scripts/residuals/ClusterDBViewer.C
@@ -2,10 +2,10 @@
 
 //
 // get histos from db
-TFile *ftb = new TFile("cal-files/vxd-s16-new/clusterDB-MC.root");
+TFile *ftb = new TFile("cal-files/vxd-s1/clusterDB-MC.root");
 
-TH1F * h_ID = (TH1F*) ftb->Get("hDB_Weight");
-h_ID->SetDirectory(0);
+TH1F * h_Weight = (TH1F*) ftb->Get("hDB_Weight");
+h_Weight->SetDirectory(0);
 
 TH1F * h_U = (TH1F*) ftb->Get("hDB_U");
 h_U->SetDirectory(0);
@@ -22,15 +22,13 @@ h_Var_V->SetDirectory(0);
 // Close root  file
 ftb->Close();
 
-cout << "total number of labels in clusterDB is " << h_ID->GetNbinsX() << endl;
-
 //
 // First, we want to compute a list of all different cluster 
 // types found in the db
 
 std::set<TString> typeset;
 
-for (int bin = 1; bin<= h_ID->GetNbinsX(); bin++) {
+for (int bin = 1; bin<= h_Weight->GetNbinsX(); bin++) {
   
   // The bin label decodes the cluster shape. The  
   // label contains tokens seperated by "D". The 
@@ -39,7 +37,7 @@ for (int bin = 1; bin<= h_ID->GetNbinsX(); bin++) {
   // seperated by "."; nameley iu, iv and signal.
   // These can be converted to integers. 
   
-  TString label(h_ID->GetXaxis()->GetBinLabel(bin));
+  TString label(h_Weight->GetXaxis()->GetBinLabel(bin));
   
   // We compute the type string by stripping all 
   // signal information from the label 
@@ -60,13 +58,35 @@ for (int bin = 1; bin<= h_ID->GetNbinsX(); bin++) {
   typeset.insert(type); 
 }
 
-cout << "total number of types in clusterDB is " << typeset.size() << endl;
+cout << "Number of labels in clusterDB is " << h_Weight->GetNbinsX() << endl;
+cout << "Number of types in clusterDB is " << typeset.size() << endl;
+
+double weightedVarU = 0;
+double weightedVarV = 0;
+double norm = 0;
+
+for (int bin = 1; bin<= h_Weight->GetNbinsX(); bin++) {
+
+  double w = h_Weight->GetBinContent(bin);
+  norm += w;
+  
+  weightedVarU += w*h_Var_U->GetBinContent(bin);
+  weightedVarV += w*h_Var_V->GetBinContent(bin);
+}
+
+if (norm >0) {
+  weightedVarU/=norm;
+  weightedVarV/=norm;
+
+  cout << "Weighted clusterDB sigmaU [mm]: " << TMath::Sqrt(weightedVarU) << endl;
+  cout << "Weighted clusterDB sigmaV [mm]: " << TMath::Sqrt(weightedVarV) << endl;
+}
 
 //
 // output root file with final cluster db histos 
-TFile *fDB = new TFile("ClusterDBViewer-s4.root","RECREATE");
+TFile *fDB = new TFile("ViewClusterDB.root","RECREATE");
 
-std::map<TString, TH1F *> histomap_id;
+std::map<TString, TH1F *> histomap_weight;
 std::map<TString, TH1F *> histomap_u;
 std::map<TString, TH1F *> histomap_v;
 std::map<TString, TH1F *> histomap_sigu;
@@ -75,14 +95,14 @@ std::map<TString, TH1F *> histomap_sigv;
 for (auto currenttype : typeset ) {
   
  
-  std::map<TString, float> map_id;
+  std::map<TString, float> map_weight;
   std::map<TString, float> map_u;
   std::map<TString, float> map_v;
   std::map<TString, float> map_sigu;
   std::map<TString, float> map_sigv;
     
 
-  for (int bin = 1; bin<= h_ID->GetNbinsX(); bin++) {
+  for (int bin = 1; bin<= h_Weight->GetNbinsX(); bin++) {
   
     // The bin label decodes the cluster shape. The  
     // label contains tokens seperated by "D". The 
@@ -91,7 +111,7 @@ for (auto currenttype : typeset ) {
     // seperated by "."; nameley iu, iv and signal.
     // These can be converted to integers. 
      
-    TString label(h_ID->GetXaxis()->GetBinLabel(bin));
+    TString label(h_Weight->GetXaxis()->GetBinLabel(bin));
     
     // We compute the type string by stripping all 
     // signal information from the label 
@@ -110,8 +130,8 @@ for (auto currenttype : typeset ) {
       }
     }
     
-    if (type == currenttype ) {
-      map_id[label] = h_ID->GetBinContent(bin);
+    if ( type == currenttype ) {
+      map_weight[label] = h_Weight->GetBinContent(bin);
       map_u[label] = h_U->GetBinContent(bin);
       map_v[label] = h_V->GetBinContent(bin);
       map_sigu[label] = TMath::Sqrt(h_Var_U->GetBinContent(bin));
@@ -125,12 +145,12 @@ for (auto currenttype : typeset ) {
 
   //
   // these are the reprocessed histos for viewing
-  int LABELS = map_id.size();
+  int LABELS = map_weight.size();
   
-  histomap_id[currenttype] = new TH1F(TString("hid_")+currenttype,"",LABELS,0,LABELS);
-  histomap_id[currenttype]->SetStats(0);
-  histomap_id[currenttype]->SetFillColor(38);
-  histomap_id[currenttype]->SetYTitle("weight");  
+  histomap_weight[currenttype] = new TH1F(TString("hweight_")+currenttype,"",LABELS,0,LABELS);
+  histomap_weight[currenttype]->SetStats(0);
+  histomap_weight[currenttype]->SetFillColor(38);
+  histomap_weight[currenttype]->SetYTitle("weight");  
 
   histomap_u[currenttype] = new TH1F(TString("hu_")+currenttype,"",LABELS,0,LABELS);
   histomap_u[currenttype]->SetStats(0);
@@ -153,20 +173,20 @@ for (auto currenttype : typeset ) {
   histomap_sigv[currenttype]->SetYTitle("cluster sigma v [mm]");  
 
   int i = 0; 
-  for (auto idAndWeight : map_id) {
+  for (auto labelAndWeight : map_weight) {
     i++;
-    TString ID = idAndWeight.first;
-    histomap_id[currenttype]->SetBinContent(i, map_id[ID]);
-    histomap_u[currenttype]->SetBinContent(i, map_u[ID]);  
-    histomap_v[currenttype]->SetBinContent(i, map_v[ID]); 
-    histomap_sigu[currenttype]->SetBinContent(i, map_sigu[ID]);  
-    histomap_sigv[currenttype]->SetBinContent(i, map_sigv[ID]); 
+    TString label = labelAndWeight.first;
+    histomap_weight[currenttype]->SetBinContent(i, map_weight[label]);
+    histomap_u[currenttype]->SetBinContent(i, map_u[label]);  
+    histomap_v[currenttype]->SetBinContent(i, map_v[label]); 
+    histomap_sigu[currenttype]->SetBinContent(i, map_sigu[label]);  
+    histomap_sigv[currenttype]->SetBinContent(i, map_sigv[label]); 
   
-    histomap_id[currenttype]->GetXaxis()->SetBinLabel(i, ID);
-    histomap_u[currenttype]->GetXaxis()->SetBinLabel(i, ID);  
-    histomap_v[currenttype]->GetXaxis()->SetBinLabel(i, ID); 
-    histomap_sigu[currenttype]->GetXaxis()->SetBinLabel(i, ID);  
-    histomap_sigv[currenttype]->GetXaxis()->SetBinLabel(i, ID); 
+    histomap_weight[currenttype]->GetXaxis()->SetBinLabel(i, label);
+    histomap_u[currenttype]->GetXaxis()->SetBinLabel(i, label);  
+    histomap_v[currenttype]->GetXaxis()->SetBinLabel(i, label); 
+    histomap_sigu[currenttype]->GetXaxis()->SetBinLabel(i, label);  
+    histomap_sigv[currenttype]->GetXaxis()->SetBinLabel(i, label); 
   }
 
 }
