627020fd804de376e7bc4124294bef49c697baa4
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2017-07-15 10:25:20 +0200

    fixup

--- source/gear/config/BuildSetup.cmake
+++ source/gear/config/BuildSetup.cmake
@@ -16,17 +16,9 @@
 # Setup project options
 #############################################################################
 
-SET( BUILD_32BIT_COMPATIBLE OFF 
-    CACHE BOOL "Set to Off for 64bit build" FORCE   )
 
 
-# Set CMAKE build type (None Debug Release RelWithDebInfo MinSizeRel), default: RelWithDebInfo
-SET( CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "Choose the type of build" FORCE )
 
-# Set basic options
-SET( BUILD_GEAR_TESTS ON CACHE BOOL  "Set to OFF not to build GEAR tests" FORCE )
-SET( INSTALL_DOC ON CACHE BOOL "Set to OFF to skip build/install Documentation" FORCE )
 
-# Set advanced options
-#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
+
 
