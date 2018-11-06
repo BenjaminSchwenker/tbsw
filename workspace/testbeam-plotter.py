cfcbea3716fdfc40e300a1eafa9f9e96abd91306
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2018-11-06 20:30:38 +0100

    mdify effi computation to require more than for digits

--- workspace/testbeam-plotter.py
+++ workspace/testbeam-plotter.py
@@ -27,7 +27,7 @@ for trackfile in glob.glob('root-files/Histos-DEPH5-{}-reco.root'.format(args.pa
     residuals.plot(inputfilename = trackfile, histofilename = ofile, basecut = "hasTrack==0", nbins=201, urange=400, vrange=400)
     
     ofile = 'Efficiency-' + os.path.basename(trackfile)  
-    efficiency.plot(inputfilename=trackfile, histofilename=ofile, basecut="trackNHits==7 && iEvt>=0 && nDutDigits>0", matchcut="hasHit==0", uaxis=(10,0,64), vaxis=(10,0,64))
+    efficiency.plot(inputfilename=trackfile, histofilename=ofile, basecut="trackNHits==7 && iEvt>=0 && nDutDigits>4", matchcut="hasHit==0", uaxis=(10,0,64), vaxis=(10,0,64))
     
   
 for trackfile in glob.glob('root-files/Histos-DEPBIG-{}-reco.root'.format(args.pattern)): 
@@ -36,7 +36,7 @@ for trackfile in glob.glob('root-files/Histos-DEPBIG-{}-reco.root'.format(args.p
     residuals.plot(inputfilename=trackfile, histofilename=ofile, basecut="hasTrack==0", nbins=201, urange=400, vrange=400)
     
     ofile = 'Efficiency-' + os.path.basename(trackfile)  
-    efficiency.plot(inputfilename=trackfile, histofilename=ofile, basecut="trackNHits==7 && iEvt>=0 && nDutDigits>0", matchcut="hasHit==0", uaxis=(100,0,250), vaxis=(100,0,768))
+    efficiency.plot(inputfilename=trackfile, histofilename=ofile, basecut="trackNHits==7 && iEvt>=0 && nDutDigits>4", matchcut="hasHit==0", uaxis=(100,0,250), vaxis=(100,0,768))
 
     # A single run will not give enough statistic for such a plot, you will need to chain tree from many runs for this
     # Anyway, here is the commond to obtain inpixel plots for the complete sensor area
