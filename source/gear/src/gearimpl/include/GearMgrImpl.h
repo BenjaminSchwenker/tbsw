35eeae566aa48cf92f53b42ff9f15437b79d68e3
Florian Luetticke <luetticke@physik.uni-bonn.de>
2019-02-06 17:39:02 +0100

    Moved Folders for flatter build tree, removed unneccecary config files

---
+++ source/gear/src/gearimpl/include/GearMgrImpl.h
@@ -0,0 +1,91 @@
+#ifndef GEAR_GearMgrImpl_H
+#define GEAR_GearMgrImpl_H 1
+
+#include <string>
+
+#include "gear/GearMgr.h"
+#include "gear/GearParameters.h"
+
+
+namespace gear {
+    
+    
+  /** Manager class that returns the Gear classes for the  relevant subdetectors.
+   * 
+   *  Based on ideas discussed at the 2004 Argonne Simulation Workshop as summarized by T.Behnke.
+   *
+   * @author F. Gaede, DESY
+   * @version $Id: GearMgrImpl.h,v 1.11 2008-10-22 15:10:46 engels Exp $
+   */
+  class GearMgrImpl : public GearMgr {
+	
+    typedef std::map< std::string ,  GearParameters* > ParameterMap ;
+
+  public: 
+
+    // C'tor 
+    GearMgrImpl() ;
+    
+    /// Destructor.
+    virtual ~GearMgrImpl() ;
+	
+   /** The unique detector name - typically the model name used in the simulation program
+    */
+    virtual const std::string& getDetectorName() const ;
+
+
+    /** Get named parameters for key. This can be used to describe a subdetector that is not 
+     *  yet forseen in the Gear API.
+     * 
+     *  @throws UnknownParameterException
+     */
+    virtual const GearParameters & getGearParameters(const std::string & key) const;
+
+    /** Get the BField.
+     */
+    virtual const BField & getBField() const ;
+
+    
+    /** Get the SiPlanes parameters.
+     *
+     *  @throws UnknownParameterException
+     */
+    virtual const SiPlanesParameters & getSiPlanesParameters() const ;
+
+    
+    /** Keys of all GearParameters. 
+     */ 
+    virtual const std::vector<std::string>  & getGearParameterKeys() const ;
+
+
+    virtual void setDetectorName(const std::string& name) { _detectorName = name ; }
+
+    /** Set the GearParameters for the given key - overwrites any 
+     *  existing entries.
+     */
+    virtual void setGearParameters( const std::string & key, GearParameters* parameters ) ;
+  
+     /** Set the SiPlanesParameters.
+     */
+    virtual void setSiPlanesParameters( SiPlanesParameters * siplanesParameters ) ;
+
+
+    /** Set the b field object
+     */
+    virtual void setBField( BField* bField ) ;
+    
+    
+    
+    
+  protected:
+    
+    ParameterMap _map ;
+    SiPlanesParameters* _siplanesParameters ;
+    BField* _bField ;
+    std::string _detectorName ;
+
+    mutable StringVec _keys ;
+
+  }; // class
+} // namespace gear
+#endif /* ifndef GEAR_GEARMGR_H */
---
+++ source/gear/src/include/gear/GearMgr.h
@@ -0,0 +1,78 @@
+
+#ifndef GEAR_GEARMGR_H
+#define GEAR_GEARMGR_H 1
+
+
+#include <string>
+#include <vector>
+
+#include "GEAR.h"
+#include "gear/GearParameters.h"
+
+namespace gear {
+
+class BField;
+class SiPlanesParameters;
+
+
+/** Abstract interface for a manager class that returns the Gear classes for the 
+ *  relevant subdetectors.
+ * 
+ *
+ * @author F. Gaede, DESY
+ * @version $Id: GearMgr.aid,v 1.9 2008-10-22 15:10:46 engels Exp $
+ */
+class GearMgr {
+
+public: 
+    /// Destructor.
+    virtual ~GearMgr() { /* nop */; }
+
+   /** The unique detector name - typically the model name used in the simulation program
+    */
+    virtual const std::string & getDetectorName() const = 0;
+
+    /** Get named parameters for key. This can be used to describe a subdetector that is not 
+     *  yet forseen in the Gear API.
+     * 
+     *  @throws UnknownParameterException
+     */
+    virtual const GearParameters & getGearParameters(const std::string & key) const = 0;
+
+    /** Get the B field map.
+     *
+     *  @throws UnknownParameterException
+     */
+    virtual const BField & getBField() const = 0;
+
+    /** Get the SiPlanes parameters.
+     *
+     *  @throws UnknownParameterException
+     */
+    virtual const SiPlanesParameters & getSiPlanesParameters() const = 0;
+
+
+    /** Keys of all GearParameters. 
+     */ 
+    virtual const std::vector<std::string>  & getGearParameterKeys() const = 0;
+
+    /** Set detector name.
+     */
+    virtual void setDetectorName(const std::string & name) = 0;
+
+    /** Set named parameters for key. This can be used to describe a subdetector that is not 
+     *  yet forseen in the Gear API.
+     */
+    virtual void setGearParameters(const std::string & key, GearParameters * gearParameters) = 0;
+
+    /** Set the BField.
+     */
+    virtual void setBField(BField * bField) = 0;
+
+    /** Set the SiPlanesParameters.
+     */
+    virtual void setSiPlanesParameters(SiPlanesParameters * siplanesParameters) = 0;
+    
+}; // class
+} // namespace gear
+#endif /* ifndef GEAR_GEARMGR_H */
