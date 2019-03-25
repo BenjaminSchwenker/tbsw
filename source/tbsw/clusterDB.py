18e53581fe429c454716d3a285ae1fa10a6923dd
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2019-03-25 17:55:04 +0100

    added possibility to compute cluster sigmas for specific pixel types and cluster shapes

--- source/tbsw/clusterDB.py
+++ source/tbsw/clusterDB.py
@@ -2,18 +2,7 @@ import re
 import ROOT
 import math
 
-shape = "E0P0.0.0D0.0"
-
-def get_labeltype(label=None): 
-  return_type = ""
-  for tok in re.split('D',label):
-    addr = re.split('\.',tok)
-    if len(addr) == 1:
-     return_type += tok    
-    else: 
-      return_type += "D" + addr[0] + '.' + addr[1]   
-    
-  return return_type
+example = 'E0P0.0.1D0.0'
 
 class ClusterDB(object):
   """
@@ -27,9 +16,65 @@ class ClusterDB(object):
     :@dbpath: path to clusterDB root file
     """    
     self.dbpath = dbpath
+    self.shapes = []
     
+    dbfile = ROOT.TFile( self.dbpath, 'READ' ) 
+    histo = dbfile.Get("hDB_Weight")
     
-  def getSigmaU(self): 
+    for bin in range(1,histo.GetNbinsX()+1):
+      shape = histo.GetXaxis().GetBinLabel(bin)
+      self.shapes.append(shape) 
+     
+    dbfile.Close()
+    
+  def getNClusters(self, shape=''): 
+    """
+    Get number of clusters used in calibration for a given selection of cluster shapes. 
+    """
+    dbfile = ROOT.TFile( self.dbpath, 'READ' ) 
+    histo  = dbfile.Get("hDB_Weight")
+     
+    nClusters = 0.0
+    
+    regex = re.compile(shape)       
+    selected_shapes = filter(regex.match, self.shapes)
+    
+    for bin in range(1,histo.GetNbinsX()+1):  
+      if histo.GetXaxis().GetBinLabel(bin) in selected_shapes:         
+        nClusters += histo.GetBinContent(bin)   
+      
+    dbfile.Close()
+    
+    return nClusters
+    
+  def getFraction(self, shape=''): 
+    """
+    Get fraction of clusters for a given selection of cluster shapes. 
+    """
+    dbfile = ROOT.TFile( self.dbpath, 'READ' ) 
+    histo  = dbfile.Get("hDB_Weight")
+     
+    fraction = 0.0
+    norm = 0.0    
+     
+    regex = re.compile(shape)       
+    selected_shapes = filter(regex.match, self.shapes)
+    
+    for bin in range(1,histo.GetNbinsX()+1):  
+      weight = histo.GetBinContent(bin)
+      norm += weight 
+      if histo.GetXaxis().GetBinLabel(bin) in selected_shapes:         
+        fraction += weight    
+      
+    dbfile.Close()
+    
+    if norm==0: 
+      return 0
+    else:  
+      return 100*fraction/norm
+    
+
+  def getSigmaU(self, shape=''): 
     """
     Get pair of sigma (and sigma error) along the u axis as weighted mean of 
     all calibrated cluster shapes in the db. 
@@ -42,20 +87,24 @@ class ClusterDB(object):
     error2= 0.0
     norm = 0.0
     
-    for bin in range(1,histo_sigmas2.GetNbinsX()+1):        
-      weight = histo_weights.GetBinContent(bin)
-      norm += weight   
-      mean2 += weight*histo_sigmas2.GetBinContent(bin)
-      error2 += weight*weight*histo_sigmas2.GetBinError(bin)
+    regex = re.compile(shape)       
+    selected_shapes = filter(regex.match, self.shapes)
+    
+    for bin in range(1,histo_sigmas2.GetNbinsX()+1):    
+      if histo_sigmas2.GetXaxis().GetBinLabel(bin) in selected_shapes:        
+        weight = histo_weights.GetBinContent(bin)
+        norm += weight   
+        mean2 += weight*histo_sigmas2.GetBinContent(bin)
+        error2 += weight*weight*histo_sigmas2.GetBinError(bin)
     
     dbfile.Close()
   
     if norm >0 and mean2>0:
       return math.sqrt(mean2/norm) , math.sqrt(error2/norm/norm)
     else: 
-      return -1, -1; 
+      return float('nan'), float('nan')
 
-  def getSigmaV(self): 
+  def getSigmaV(self, shape=''): 
     """
     Get pair of sigma (and sigma error) along the v axis as weighted mean of 
     all calibrated cluster shapes in the db. 
@@ -68,17 +117,21 @@ class ClusterDB(object):
     error2= 0.0
     norm = 0.0
     
-    for bin in range(1,histo_sigmas2.GetNbinsX()+1):        
-      weight = histo_weights.GetBinContent(bin)
-      norm += weight   
-      mean2 += weight*histo_sigmas2.GetBinContent(bin)
-      error2 += weight*weight*histo_sigmas2.GetBinError(bin)
+    regex = re.compile(shape)       
+    selected_shapes = filter(regex.match, self.shapes)
+     
+    for bin in range(1,histo_sigmas2.GetNbinsX()+1):  
+      if histo_sigmas2.GetXaxis().GetBinLabel(bin) in selected_shapes:        
+        weight = histo_weights.GetBinContent(bin)
+        norm += weight   
+        mean2 += weight*histo_sigmas2.GetBinContent(bin)
+        error2 += weight*weight*histo_sigmas2.GetBinError(bin)
     
     dbfile.Close()
-  
+    
     if norm >0 and mean2>0:
       return math.sqrt(mean2/norm) , math.sqrt(error2/norm/norm)
     else: 
-      return -1, -1; 
-
+      return float('nan'), float('nan') 
+    
     
--- workspace/clusterDB.py
+++ workspace/clusterDB.py
@@ -22,11 +22,40 @@ for expName in expdb.keys():
   
   pxdDB = tbsw.clusterDB.ClusterDB("localDB/{:s}/clusterDB-PXD.root".format(expName))
   
-  sigU, sigUError = pxdDB.getSigmaU()
-  sigV, sigVError = pxdDB.getSigmaV()
-  
-  print("  SigmaU={:.4f}+/- {:.5f}mm".format(sigU, sigUError))
-  print("  SigmaU={:.4f}+/- {:.5f}mm".format(sigV, sigVError))
+  for pixelType in range(2):
+    
+    # You can querry the ClusterDB to get estimates for the spatial 
+    # resolution (sigma's) for a specific cluster shape. 
+    # You can use regular expression for shapes to get the sigmas  
+    # on different levels of granularity. 
+    #
+    # 0) Example: '^E{:d}P{:d}.{:d}.{:d}D0.0$'.format(etaBin, vPeriod, uPeriod, pixelType)
+    #    
+    # You can specify the etaBin, vPeriod and uPeriod and pixelType to distinguish clusters
+    # from different regions of the sensor. You also request cluster with exactly one firing pixel. 
+    #
+    # 1) Example: '^E[0-9]+P[0-9]+.[0-9]+.{:d}'.format(pixelType) 
+    #    
+    # You can specify the pixelType to distinguish between different a region with a specific  
+    # pixel pitch. All values for etaBin, vPeriod, uPeriod and all arrangements of firing
+    # pixels are matched.   
+    # 
+    # 2) Example: '^E[0-9]+P[0-9]+.[0-9]+.1D0.0D0.1$'
+    #
+    # You can select the pixel type (=1) and the detailed arrangement of 
+    # fired pixels in the cluster (exactly two pixels in the same column).
+    
+    # Select a shape pattern
+    shape = '^E[0-9]+P[0-9]+.[0-9]+.{:d}'.format(pixelType) 
+           
+    sigU, sigUError = pxdDB.getSigmaU(shape)
+    sigV, sigVError = pxdDB.getSigmaV(shape)
+    frac = pxdDB.getFraction(shape) 
+    
+    print("  PixelType={:d}".format(pixelType))
+    print("  Fraction={:.3f}%".format(frac))
+    print("  SigmaU={:.4f}+/- {:.5f} mm".format(sigU, sigUError))
+    print("  SigmaV={:.4f}+/- {:.5f} mm".format(sigV, sigVError))
     
     
     
